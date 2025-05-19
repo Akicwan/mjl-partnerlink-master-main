49c40fec89a1b4a36ee337905cbc24ad16258e6f
Haziq <haziqrashid20@gmail.com>
2025-05-20 06:27:42 +0800

    fixed the notif button link

--- src/app/notiftestPartner/page.jsx
+++ src/app/notiftestPartner/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
-import Sidebar from '../components/SidebarPartner';
+import Sidebar from '../components/Sidebar';
 
 export default function PartnerNotification() {
   const [notifications, setNotifications] = useState([]);
